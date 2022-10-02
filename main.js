3e1804d598415d76690c9aa676a969a3b6a81eaa
Tanya <facetimewithmymama@gmail.com>
2022-10-02 20:37:26 +0300

    usloviya

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
-const numberOfFilms = +prompt('Сколько  фильмов вы уже посмотрели?', ' ');
+"use strict"
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+if (4 == 9) {
+    console.log('ok');
+} else {
+    console.log('error')
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ' '),
-      b = prompt('На сколько оцените его?', ' '),
-      c = prompt('Один из последних просмотренных фильмов?', ' '),
-      d = prompt('На сколько оцените его?', ' ');
+const num = 101;
+if (num < 49) {
+    console.log('error')
+} else if (num > 100) {
+    console.log('mnogo')
+} else {
+    console.log('ok')
+};
 
-     personalMovieDB.movies[a] = b; 
-     personalMovieDB.movies[c] = d; 
 
-     console.log(personalMovieDB);
+(num === 50) ? console.log('ok') : console.log('error');
+
+const NUM = 55;
+
+switch (NUM) {
+    case 49:
+        console.log('neverno');
+        break;
+        case 100:
+            console.log('neverno');
+            break;
+            case 50:
+                console.log('v tochku');
+                break;
+                default:
+                    console.log('ne v etot raz');
+                break;
+}
