fca03f68171f2849b48dce06ddf5487239d0666f
Tanya <facetimewithmymama@gmail.com>
2022-10-08 17:01:32 +0300

    function

--- main.js
+++ main.js
@@ -1,89 +1,28 @@
 "use strict";
 
-// for (let i = 0; i < 3; i++) {
-//     console.log(i);
-//     for (let j = 0; j < 3; j++) {
-//         console.log(j);
-//     }
-// }
+let num = 20;
 
-// // let result = '';
-// // const length = 8;
+function showFirstMessage(text) {
+    console.log(text);
+     num = 10;
+}
 
-// // for (let i = 1; i < length; i++) {
-// //     for (let j = 0; j < i; j++) {
-// //         result += "*";
-// //     }
+showFirstMessage('hello');
+console.log(num);
 
+function calc(a, b) {
+    return (a + b);
+}
 
-// //     result += '\n';
-// // }
-
+console.log(calc(5, 6));
+console.log(calc(10, 6));
+console.log(calc(13, 25));
 
 
-
-
-// // console.log(result);
-
-// let result = '';
-// const length = 7;
-// for (let i =  1; i < 7; i++) {
-//     for (let j = 0; j < i; j++) {
-//         result += "*";
-//     }
-//     result += '\n';
-// }
-
-// console.log(result);
-
-
-// let result = ' ';
-// const length = 7;
-
-// for (let i = 1; i < 7; i++) {
-//     for (let j = 0; j < i; j++) {
-//         result += "*";
-//     }
-//     result += "\n";
-// }
-
-// console.log(result);
-
-
-const numberOfFilms = +prompt('Сколько  фильмов вы уже посмотрели?', ' ');
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+const logger = function() {
+    console.log('hello');
 };
 
+logger();
 
-
-     for (let i = 0; i <2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ' '),
-              b = prompt('На сколько оцените его?', ' ');
-
-              if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-                personalMovieDB.movies[a] = b;
-                console.log('done');
-              }  else {
-                 console.log('error');
-                 i--;
-              }
-
-     }
-
-     if (personalMovieDB.count < 10) {
-        console.log('Просмотренно довольно мало фильмов');
-     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-console.log("вы классический зритель");
-     } else if (personalMovieDB.count >= 30) {
-        console.log("вы киноман");
-     } else {
-        console.log('произошла ошибка');
-     }
-
-     console.log(personalMovieDB);
+const calc = (a, b) => a + b;
