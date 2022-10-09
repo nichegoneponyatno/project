297b1ba937c3758479971ca6fa300a789cbf74e2
Tanya <facetimewithmymama@gmail.com>
2022-10-09 17:03:44 +0300

    metod

--- main.js
+++ main.js
@@ -1,65 +1,25 @@
 "use strict";
 
-// let num = 20;
+// const str = "test";
 
-// function showFirstMessage(text) {
-//     console.log(text);
-//      num = 10;
-// }
+// console.log(str.length);
 
-// showFirstMessage('hello');
-// console.log(num);
-
-// function calc(a, b) {
-//     return (a + b);
-// }
-
-// console.log(calc(5, 6));
-// console.log(calc(10, 6));
-// console.log(calc(13, 25));
+const fruit = 'some fruit';
+console.log(fruit.indexOf('i'));
 
 
-// const logger = function() {
-//     console.log('hello');
-// };
+const logg = 'hello world';
+console.log(logg.slice(6, 11));
 
-// logger();
+console.log(logg.substring(6, 11));
 
-// const calc = (a, b) => a + b;
+console.log(logg.substr(6, 3));
 
+const num = 12.2;
 
-// const usdCurr = 28;
-// const eurCurr = 32;
-
-// function convert(amount, curr) {
-//     console.log(curr * amount);
-// }
-// convert(500, usdCurr);
-// convert(500, eurCurr);
-
-const usdCurr = 28;
-const discount = 0.9;
-
-function convert(amount, curr) {
-    return curr * amount;
-}
-
-function promotion(result) {
-    console.log(result * discount);
- 
-}
-
-promotion(convert(500, usdCurr));
-
-function test() {
-    for (let i = 0; i < 5; i++) {
-        console.log(i);
-        if (i === 3) return
-    }
-    console.log('done')
-}
-test();
+console.log(Math.round(num));
 
 
-function doNothing() {};
-    console.log(doNothing() === undefined);
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
