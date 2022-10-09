1f19cb27a9ae260a206712b642d1d96de76178c5
Tanya <facetimewithmymama@gmail.com>
2022-10-09 15:30:28 +0300

    function2

--- main.js
+++ main.js
@@ -1,28 +1,65 @@
 "use strict";
 
-let num = 20;
+// let num = 20;
 
-function showFirstMessage(text) {
-    console.log(text);
-     num = 10;
+// function showFirstMessage(text) {
+//     console.log(text);
+//      num = 10;
+// }
+
+// showFirstMessage('hello');
+// console.log(num);
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+// console.log(calc(13, 25));
+
+
+// const logger = function() {
+//     console.log('hello');
+// };
+
+// logger();
+
+// const calc = (a, b) => a + b;
+
+
+// const usdCurr = 28;
+// const eurCurr = 32;
+
+// function convert(amount, curr) {
+//     console.log(curr * amount);
+// }
+// convert(500, usdCurr);
+// convert(500, eurCurr);
+
+const usdCurr = 28;
+const discount = 0.9;
+
+function convert(amount, curr) {
+    return curr * amount;
 }
 
-showFirstMessage('hello');
-console.log(num);
-
-function calc(a, b) {
-    return (a + b);
+function promotion(result) {
+    console.log(result * discount);
+ 
 }
 
-console.log(calc(5, 6));
-console.log(calc(10, 6));
-console.log(calc(13, 25));
+promotion(convert(500, usdCurr));
+
+function test() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+        if (i === 3) return
+    }
+    console.log('done')
+}
+test();
 
 
-const logger = function() {
-    console.log('hello');
-};
-
-logger();
-
-const calc = (a, b) => a + b;
+function doNothing() {};
+    console.log(doNothing() === undefined);
